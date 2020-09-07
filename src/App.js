c9acf07b20daf133687c81c6436da9046a65c7fd
ipul1705 <saifurrohman1705@gmail.com>
2020-09-07 10:08:27 +0700

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+  constructor(props){
+      super(props)
+
+      this.state = {
+
+        items : [],
+        isLoading : true
+
+      }
+  }
+
+  componenDidMount(){
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json)
+      .then(data => this.setState({items:data,isLoading:false}))
+  }
+
+  render(){
+    const {items,isLoading} = this.state
+  
+
+    if(isLoading){
+      return <p>Loading...</p>
+    }
+
+    return (
+
+      <div>
+        <ul>
+        { items.map((item,index) => <li key={index}> {item.name}</li>) }
+        </ul>
+      </div>
+
+    );
+    }
 }
 
 export default App;
